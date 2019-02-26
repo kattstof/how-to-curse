7108948a425830cc9c8d52010908a56a101095d9
Katt Stof <katrinastofferman@autistici.org>
2019-02-25 19:37:17 -0800

    added option to add new curseword

--- curseword.py
+++ curseword.py
@@ -1,4 +1,12 @@
 import random
-curses = open("curses.txt").readlines()
+curses = open("curses.txt",).readlines()
 sentences = open("sentences.txt").readlines()
 print(random.choice(sentences),(random.choice(curses))) 
+new = input("Please type a new curseword/enter to exit.:")
+if new != "":
+    with open("curses.txt", "a") as f:
+        f.write(new)
+elif new == "":
+    print("Goodbye, Twatwaffle")
+    exit()
+
